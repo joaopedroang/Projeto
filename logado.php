d0a0455e4de414b3d9d3a0dd2b9de77acdcb04b8
João Pedro <127750232+joaopedroang@users.noreply.github.com>
2023-05-23 11:21:45 -0300

    Add files via upload

--- logado.php
+++ logado.php
@@ -1,1 +1,11 @@
+<?php
+include ('conexao.php');
+$login = isset ($_POST ['login'])?$_POST['login'] : ''; 
+$senha = isset ($_POST ['senha'])?$_POST['senha'] : '';
 
+if ($login=='aluno' && $senha=='fpb') {
+    echo "Dados corretos";
+} else {
+    echo "Dados errados";
+}
+?>

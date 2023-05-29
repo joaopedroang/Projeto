11b6104592f1dff6bf1972b4293087396ddc1179
João Pedro <127750232+joaopedroang@users.noreply.github.com>
2023-05-29 07:27:59 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php
-session_start();
-echo $_SESSION['nome'];
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="estilos/style.css">
+</head>
+<body>
+    <main>
+        <section id="login">
+            <div id="imagem">
+
+            </div>
+            <div id="formulario">
+                <h1>Login</h1>
+                <p>Seja bem vindo(a)!</p>
+                <form action="login.php" method="post">
+
+                </form>
+            </div>
+        </section>
+    </main>
+</body>
+</html>

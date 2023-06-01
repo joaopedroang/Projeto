709a9426d9df92ec808c531e9dfdd662f371ef1f
João Pedro <127750232+joaopedroang@users.noreply.github.com>
2023-06-01 11:49:32 -0300

    Update login.php

--- login.php
+++ login.php
@@ -11,8 +11,8 @@
     <center>
         <h2>Olá, visitante</h2><br><br>
         <form id="login" action="logado.php" method="POST">
-            Login: <input type="text" name="login"><br>
-            Senha: <input type="password" name ="senha"><br><br>
+            Login: <input type="text" name="login" required><br>
+            Senha: <input type="password" name ="senha" required><br><br>
             <input type = "submit" name = "entrar" value = "Entrar">
         </form>
     </center>
